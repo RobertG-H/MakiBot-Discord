bbe68d7eeea66767714c2715afcee6491d666f2b
RobertG-H <robertgrahamhu@gmail.com>
2018-06-29 19:53:48 -0400

    Finished photo posting to anime pics channel

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// https://discordapp.com/oauth2/authorize?client_id=457012879594618882&scope=bot4
+// https://discordapp.com/api/oauth2/authorize?client_id=457012879594618882&permissions=19472&scope=bot
+
 require('dotenv').config(); // For process.env to work
 
 // Discord bot stuff
@@ -11,7 +14,8 @@ var url = require('url');
 var siteUrl = process.env.SITE_URL;
 
 // Custom channel stuff
-var NotifyChannel;
+var NotifyChannels = [];
+//var servers = {};
 
 // Scheduler stuff
 var cron = require('node-cron');
@@ -20,11 +24,14 @@ var cron = require('node-cron');
 
 // Bot setup
 bot.on("ready", async () => {
-  //var server = guild;
   console.log(`${bot.user.username} is online!`);
-  bot.user.setGame("Lewd the dragon loli");
-  //server.createChannel("anime-pics", "text");
-  NotifyChannel = bot.channels.find("name", "general"); //MIGHT NEED TO BE CHANGED
+  bot.user.setActivity("Lewding all dragon lolis");
+});
+
+bot.on("guildCreate", guild => {
+  console.log(`Just joined: {guild.name}`);
+  addPostChannel (guild, "general");
+  NotifyChannel.send('Hiya! I am Makibot use the "~help" command for a list of things you can do with me.')
 });
 
 // Message commands (prefix: ~)
@@ -35,6 +42,9 @@ bot.on("message", async message => {
   let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
+  let input = null;
+  if (messageArray[1] != null)
+    input = messageArray[1];
   let args = messageArray.slice(1);
 
   // Hello command
@@ -52,8 +62,34 @@ bot.on("message", async message => {
     return message.channel.send(botembed);
   }
 
+  // Help command
   if(cmd === `${prefix}help` || cmd === `${prefix}Help`) return message.channel.send(
-    "Hiya! My name is Makibot. I post lewd photos just in the general channel for you at 8am and 8pm everyday ( ͡° ͜ʖ ͡°)");
+    "Hiya! \n\nMy name is Makibot. I post lewd photos in a the anime-pics channel (make sure you have one) just for you at 8am and 8pm everyday ( ͡° ͜ʖ ͡°) \n\n(Sorry I don't have any commands right now)");
+
+  // Creates and joins new posting channel
+  // TODO Add database support to store the posting channel per guild
+  /*if(cmd === `${prefix}createpostchannel`){
+    if(input == null){
+      console.log("No channel name");
+      return message.channel.send ("Enter a channel name baka!~");
+    }
+     var server = message.guild;
+     server.createChannel (input, "text");
+     addPostChannel (server, input);
+     return message.channel.send (`I just created a new channel: "${input.toString()}"  I will post there from now on.`);
+  }
+*/
+  // Sets channel to post pics to
+/*
+  if(cmd === `${prefix}setpostchannel`){
+    if(input == null){
+      console.log("No channel name");
+      return message.channel.send ("Enter a channel name baka!~");
+    }
+    var server = message.guild;
+    addPostChannel(server, input);
+    return message.channel.send (`I will post to "${input.toString()}" from now on.`);
+  } */
 
   // Post Picture command
   if(cmd === `${prefix}post`){
@@ -68,7 +104,23 @@ function sendPicture () {
       var importedJSON = JSON.parse(body);
       var imageURL = importedJSON.data.children[0].data.url;
       console.log("POSTING...")
-      NotifyChannel.send(imageURL);
+      // Setting all posting channels
+      NotifyChannels = [];
+      bot.guilds.map((guild) => {
+        guild.channels.map((channel) => {
+          if (channel.type === 'text') {
+            if(channel.name === 'anime-pics'){
+              //console.log(`Guild: ${guild.id} and channel: ${channel.name}`);
+              NotifyChannels.push(channel);
+            }
+          }
+        });
+      });
+      //Loop through each channel and post
+      NotifyChannels.forEach( NotifyChannel => {
+        NotifyChannel.send(imageURL);
+      });
+
     }
     else {
       console.log("ERROR READING JSON");
@@ -77,10 +129,15 @@ function sendPicture () {
   });
 }
 
+/*
+function addPostChannel (server, channelName) {
+  console.log("Setting channel to post in to: ", channelName);
+  NotifyChannel = server.channels.find("name", channelName);
+} */
+
 // Scheduler
-cron.schedule('20,21,22 * * * *', function(){
-  console.log('running every minute 20');
-  sendPicutre ();
+cron.schedule('* 8,20 * * *', function(){
+  sendPicture ();
 });
 
 bot.login(process.env.DISCORD_TOKEN);
