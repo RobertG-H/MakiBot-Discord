9c12a3ab1bbbaedd5257d09407b71ae403d69880
RobertG-H <robertgrahamhu@gmail.com>
2018-06-30 01:13:46 -0400

    Added Listening for express app

--- index.js
+++ index.js
@@ -145,8 +145,12 @@ bot.login(process.env.DISCORD_TOKEN);
 // Self pinging
 var express = require('express');
 var app = express();
-app.set('port', (process.env.PORT|| 5000));
 var http = require("http");
+app.set('port', (process.env.PORT|| 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 setInterval(function() {
     http.get("http://maki-bot-discord.herokuapp.com");
 }, 300000);
